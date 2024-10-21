a9782ca32f41df85bee0d88938789df7436f6e05
Ali Akbaş <79078268+0aliakbas0@users.noreply.github.com>
2024-10-21 22:08:51 +0300

    Add files via upload

---
+++ karsilastir.py
@@ -0,0 +1,10 @@
+Sayi1 = input("1.Sayıyı Giriniz")
+Sayi2 = input("2.Sayıyı Giriniz")
+
+if Sayi1 > Sayi2:
+    print("Doğru Cevap Aşağıda")
+    print(Sayi1)
+
+else:
+     print("Doğru Cevap Aşağıda")
+     print(Sayi2 )
